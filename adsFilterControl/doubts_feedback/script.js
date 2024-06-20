45dba9f8d3584d390e763c2c6e603c38b7b9b3e0
Didira123 <ogeiddiego064@gmail.com>
2024-06-20 10:30:16 -0300

    Ações:Implementação da tela "doubts_feedback" da extensão "Ads Filter Control";Status:feito;(mais info: arquivo 'changelog.txt')

--- adsFilterControl/doubts_feedback/script.js
+++ adsFilterControl/doubts_feedback/script.js
@@ -24,7 +24,7 @@ document.addEventListener(`DOMContentLoaded`, () => {
     const locale_text = lt.getAttribute(`locale_text`);
     const locale_text_value = messages[locale_text]?.message;
     const value = locale_text_value ? locale_text_value : `NO_VALUE`;
-    if (locale_text.startsWith(`reason-`)) {
+    if (locale_text.includes(`-reason_type-`)) {
       lt.previousElementSibling.value = value;
       lt.textContent = value;
     } else if (lt.tagName === `TEXTAREA`) {
@@ -38,7 +38,7 @@ document.addEventListener(`DOMContentLoaded`, () => {
   if (image) {
     image.alt = messages[`image_icon_alt`]?.message || `NO_VALUE`;
   }
-  errorMessage = messages[`uninstalled-form-error`]?.message || `NO_VALUE`;
+  errorMessage = messages[`doubts_feedback-error`]?.message || `NO_VALUE`;
   // Execution that updates the lang attribute in the html tag
   document.documentElement.lang = locale;
 });
@@ -47,42 +47,28 @@ if (image) {
   image.src = `../images/icon.png`;
 }
 
-// Execution that checks if the checked input has the `ask_for_details` parameter, to then display the textarea field
-$All(`input[name="reason"]`).forEach(inp => {
-  if (inp.checked && inp.getAttribute(`ask_for_details`)) {
-    $(`textarea[name="more_details"]`).style.display = `block`;
-  }
-  inp.addEventListener(`click`, (event) => {
-    $(`textarea[name="more_details"]`).style.display = (event.target.getAttribute(`ask_for_details`)) ? `block` : `none`;
-  })
-});
-
 // Function that verifies and "submits" an email pattern
 function verifyAndSubmitEmail(event) {
   event.preventDefault();
-  const reasons = event.target.reason;
-  let checkedReason = null;
-  for (r of reasons) {
-    if (r.checked) {
-      checkedReason = r;
+  const reason_types = event.target.reason_type;
+  let checked_reason_type = null;
+  for (rt of reason_types) {
+    if (rt.checked) {
+      checked_reason_type = rt.value;
       break;
     }
   }
-  let more_details = null;
-  if (checkedReason && checkedReason.getAttribute(`ask_for_details`)) {
-    more_details = $(`textarea[name="more_details"]`).value;
-  }
   const questions_and_feedback = event.target.questions_and_feedback.value;
-  if (!checkedReason && !questions_and_feedback) {
+  if (!questions_and_feedback || !checked_reason_type) {
     alert(errorMessage);
     return;
   }
-  const reason = checkedReason ? messages[`reason`]?.message : ``;
-  const questionsFeedback = questions_and_feedback ? messages[`questions_and_feedback`]?.message : ``;
-  const extension_uninstalled = messages[`extension_uninstalled`]?.message;
-  const emailTo = `didira123321aridid@gmail.com`;
-  const subject = `${extension_uninstalled} ${reason ? `- ` + reason : ``}${questionsFeedback ? `- ` + questionsFeedback : ``}`;
-  const body = `${checkedReason ? `${reason}: ${checkedReason.value}\n${more_details ? more_details + `\n` : ``}` : ``}${questions_and_feedback ? `${questionsFeedback}: ${questions_and_feedback}` : ``}`;
+
+  const extension_doubts_feedback = messages[`extension_doubts_feedback`]?.message;
+  const emailTo = `didira123321aridid@gmail.com, kaironlorenzo118@gmail.com`;
+  const subject = `${extension_doubts_feedback} - ${checked_reason_type}`;
+  const body = `${checked_reason_type}: ${questions_and_feedback}`;
+  // alert(`mailto:${emailTo}?subject=${subject}&body=${body}`);
   window.location.href = `mailto:${emailTo}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
 }
 
--- adsFilterControl/translations.js
+++ adsFilterControl/translations.js
@@ -3,6 +3,9 @@ const translations = {
     "help_us_improve": {
       "message": "Help us improve Ads Filter Control!"
     },
+    "doubts_and_feedback": {
+      "message": "Doubts and Feedback"
+    },
     "image_icon_alt": {
       "message": "'Ads Filter Control' Icon"
     },
@@ -11,6 +14,39 @@ const translations = {
     },
     "hope_you_come_back-text": {
       "message": "We would like to thank you for using Ads Filter Control. Your opinion is very important to us. If you liked Ads Filter Control, please consider leaving a review. Your feedback helps us improve and offer an even better service. If you have any suggestions or encounter any problems, please do not hesitate to contact us."
+    },
+    "faq": {
+      "message": "Frequently Asked Questions - FAQ"
+    },
+    "faq-1": {
+      "message": "Extension Activation"
+    },
+    "faq-1-text": {
+      "message": "After downloading, opening the extension allows you to choose whether you want to filter ads by links or categories, applying your customizations. Options include blocking all ads or blocking by filter, with the default option being blocking all ads."
+    },
+    "faq-2": {
+      "message": "Filter Ads by Link"
+    },
+    "faq-2-text": {
+      "message": "To filter ads by link, simply obtain the link by clicking on the extension icon that appears during the ad to save it. The extension then allows you to select whether to block or allow it. If the extension is set to block links, the ad will be automatically skipped after the click."
+    },
+    "faq-3": {
+      "message": "Filter Ads by Category"
+    },
+    "faq-3-text": {
+      "message": "By default, the extension has a predefined list of 100 ad categories. If an ad from an unrecognized category appears, the extension will automatically save that category as a new one, making it possible to use it as a parameter for permission and blocking. After selecting the categories you want, which must be selected through one click, you must also click on the \"Update Data\" button for the changes to be saved. It is also possible to add new categories through the bar by typing your own category and then pressing \"Enter\" to add it. Don't forget to save the updates so they can be applied."
+    },
+    "faq-4": {
+      "message": "Allow and Block Buttons"
+    },
+    "faq-4-text": {
+      "message": "In the list of categories and links there is a button that can be changed between allow and block. When the button is on lock, all selected categories/links will not be displayed. When changed to allow, only selected categories/links will be displayed, with the ability to switch between links and categories. If a category is allowed, it is possible to block specific ads from this category in the links section."
+    },
+    "faq-5": {
+      "message": "Uninstallation of the extension"
+    },
+    "faq-5-text": {
+      "message": "When you uninstall the extension, all data is automatically removed. This includes any previously defined settings or filters, preventing unnecessary accumulation of data in the user's browser."
     },
     "why_removing_extension": {
       "message": "Why are you removing the extension?"
@@ -84,8 +120,17 @@ const translations = {
     "questions_and_feedback": {
       "message": "Questions and Feedback"
     },
-    "questions_and_feedback-text": {
+    "questions_and_feedback-text-1": {
+      "message": "Didn't find the answers you were looking for? Select the reason for contact and send us a message:"
+    },
+    "questions_and_feedback-text-2": {
       "message": "Ask your questions and provide feedback..."
+    },
+    "questions_and_feedback-reason_type-1":{
+      "message":"Questions"
+    },
+    "questions_and_feedback-reason_type-2":{
+      "message":"Bug Reporting"
     },
     "send": {
       "message": "Send"
@@ -105,13 +150,22 @@ const translations = {
     "reason": {
       "message": "Reason"
     },
-    "form_error": {
+    "uninstalled-form-error": {
       "message": "Error: fill in at least one of the fields."
-    }
+    },
+    "extension_doubts_feedback": {
+      "message": "Ads Filter Control Doubts/Feedback"
+    },
+    "doubts_feedback-error": {
+      "message": "Error: fill in the \"Questions and Feedback\" field"
+    },
   },
   "pt": {
     "help_us_improve": {
       "message": "Ajude-nos a melhorar o Ads Filter Control!"
+    },
+    "doubts_and_feedback": {
+      "message": "Dúvidas e Feedback"
     },
     "image_icon_alt": {
       "message": "Ícone da 'Ads Filter Control'"
@@ -121,6 +175,39 @@ const translations = {
     },
     "hope_you_come_back-text": {
       "message": "Gostaríamos de agradecer por usar a Ads Filter Control. Sua opinião é muito importante para nós. Se você gostou da Ads Filter Control, por favor, considere deixar uma avaliação. Seu feedback nos ajuda a melhorar e a oferecer um serviço ainda melhor. Se tiver qualquer sugestão ou encontrar algum problema, não hesite em nos contatar."
+      },
+    "faq": {
+      "message": "Dúvidas Frequentes - FAQ"
+    },
+    "faq-1": {
+      "message": "Ativação da Extensão"
+    },
+    "faq-1-text": {
+      "message": "Após o download, abrir a extensão permite escolher se deseja filtrar os anúncios por links ou categorias, aplicando suas personalizações. As opções incluem bloquear todos os anúncios ou bloquear por filtro, sendo a opção padrão de bloquear todos os anúncios."
+    },
+    "faq-2": {
+      "message": "Filtrar Anúncios por Link"
+    },
+    "faq-2-text": {
+      "message": "Para filtrar anúncios por link, basta obter o link clicando no ícone da extensão que aparecera durante o anuncio para que seja salvo. A extensão permite então a seleção de bloqueio ou permissão do mesmo. Caso a extensão esteja definida para bloquear os links, o anúncio será pulado automaticamente após o clique."
+    },
+    "faq-3": {
+      "message": "Filtrar Anúncios por Categoria"
+    },
+    "faq-3-text": {
+      "message": "Por padrão, a extensão possui uma lista predefinida de 100 categorias de anúncios. Caso algum anúncio de uma categoria não reconhecida venha a aparecer, a extensão automaticamente salvará essa categoria como nova, sendo possível utilizá-la como parâmetro para permissão e bloqueio. Após selecionar as categorias que deseja, sendo necessário a seleção através de um clique, é necessário clicar também no botão \"Atualizar Dados\" para que as alterações sejam salvas. Também e possível incluir categorias novas através da barra digitando sua própria categoria e em seguida pressionando \"Enter\" para adicioná-la. Não esqueça de salvar as atualizações para que sejam aplicadas."
+    },
+    "faq-4": {
+      "message": "Botões de Permissão e Bloqueio"
+    },
+    "faq-4-text": {
+      "message": "Na lista de categorias e links existe um botão que pode ser alterado entre permitir e bloquear. Quando o botão está em bloquear, todas categorias/links selecionados não serão exibidos. Quando mudado para permitir, apenas categorias/links selecionados serão exibidos, podendo intercalar entre links e categorias. Se alguma categoria estiver permitida, é possível bloquear anúncios específicos desta categoria na parte de links."
+    },
+    "faq-5": {
+      "message": "Desinstalação da Extensão"
+    },
+    "faq-5-text": {
+      "message": "Ao desinstalar a extensão, todos os dados são automaticamente removidos. Isso inclui qualquer configuração ou filtro previamente definido, evitando acumulação desnecessária de dados no navegador do usuário."
     },
     "why_removing_extension": {
       "message": "Por que está removendo a extensão?"
@@ -194,8 +281,17 @@ const translations = {
     "questions_and_feedback": {
       "message": "Perguntas e Feedback"
     },
-    "questions_and_feedback-text": {
+    "questions_and_feedback-text-1": {
+      "message": "Não encontrou as respostas que procurava? Selecione o motivo do contato e nos envie uma mensagem:"
+    },
+    "questions_and_feedback-text-2": {
       "message": "Diga suas perguntas e forneça feedback..."
+    },
+    "questions_and_feedback-reason_type-1":{
+      "message":"Perguntas"
+    },
+    "questions_and_feedback-reason_type-2":{
+      "message":"Reporte de erros"
     },
     "send": {
       "message": "Enviar"
@@ -215,8 +311,14 @@ const translations = {
     "reason": {
       "message": "Razão"
     },
-    "form_error": {
+    "uninstalled-form-error": {
       "message": "Erro: preencha ao menos um dos campos."
-    }
+    },
+    "extension_doubts_feedback": {
+      "message": "Ads Filter Control Dúvidas/Feedback"
+    },
+    "doubts_feedback-error": {
+      "message": "Erro: preencha o campo de \"Perguntas e Feedback\""
+    },
   }
 };
